6cd8baa7ad3b336652af2320743ad6c6b913fefb
Eduardo Lemos <eddu1930@gmail.com>
2019-12-30 15:54:37 -0500

    Update

--- Sorting Algorithms/bubbleSort.js
+++ Sorting Algorithms/bubbleSort.js
@@ -2,10 +2,12 @@
 // Which takes in an array and returns the sorted ascending/numeric order of it
 
 function bubbleSort(arr) {
+    let noSwaps;
     // Reason why we do this loop with i-- is so we don't repeat the whole loop over and over
     // Else the loop would go over 4 and return undefined
     // 'i' starts at arr.length
     for (let i = arr.length; i > 0; i--) {
+        noSwaps = true; // This is so we can break out of loop later if no more swaps
         // Now we iterate j over (i - 1)
         for (let j = 0; j < i - 1; j++) {
             console.log(arr, arr[j], arr[j+1]);
@@ -15,9 +17,11 @@ function bubbleSort(arr) {
                 let temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                noSwaps = false; // Basically if we never reach this point, noSwap will be true
             }
         }
         console.log("THIS PASS IS COMPLETE") // Each time this is completed, 'i' decreases.
+        if(noSwaps) break;
     }
     return arr;
 }
