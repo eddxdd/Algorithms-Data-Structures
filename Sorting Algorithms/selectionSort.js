1ea9dbc0ee954b1ea36970a55eaa75adefdb438d
Eduardo Lemos <eddu1930@gmail.com>
2019-12-30 17:16:53 -0500

    Update

---
+++ Sorting Algorithms/selectionSort.js
@@ -0,0 +1,24 @@
+// Write a function called selectionSort()
+// Takes an array, and returns sorted array
+// Selection Sort finds a minimum, swaps at the end of loop and put it at the beginning
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        // Only swap if i is not min
+        if (i !== min) {
+            // Swap
+            let temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+    return arr;
+}
+
+console.log(selectionSort([35,20,10,15,25])); // [10,15,20,25,35]
