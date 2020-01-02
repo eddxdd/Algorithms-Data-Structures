d34592a34f3fd443014cbb45e26d44defa80f4ad
Eduardo Lemos <eddu1930@gmail.com>
2020-01-02 15:29:27 -0500

    Update

---
+++ Sorting Algorithms/quickSort.js
@@ -0,0 +1,10 @@
+// Write a function called quickSort()
+// Which accepts two arrays, and returns the sorted array
+// For quick sort: split -> sort -> merge. 
+// For quick sort you must pick a pivot element, and place all lesser numbers to the left, and all greater numbers to the right.
+
+function pivot(arr, start=0, end=arr.length-1) {
+
+}
+
+console.log(pivot([4,8,2,1,5,7,6,3])); // Picking '4' should return index of 3
