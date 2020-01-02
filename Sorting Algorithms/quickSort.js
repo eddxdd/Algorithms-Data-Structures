d0ed86da5c134add71baf7d3f1aea1bb130a74db
Eduardo Lemos <eddu1930@gmail.com>
2020-01-02 16:24:46 -0500

    Update

--- Sorting Algorithms/quickSort.js
+++ Sorting Algorithms/quickSort.js
@@ -4,7 +4,31 @@
 // For quick sort you must pick a pivot element, and place all lesser numbers to the left, and all greater numbers to the right.
 
 function pivot(arr, start=0, end=arr.length-1) {
+    function swap(array, i, j) {
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 
+    let pivot = arr[start];
+    // This variable keeps track of where will we swap our pivot to
+    // Starts at start
+    let swapIdx = start; 
+
+    // Start at +1 because there's no need to compare start to itself
+    for (let i = start + 1; i < arr.length; i++) {
+        if (pivot > arr[i]) {
+            // If there's something less than 'start'
+            // Move swapIdx up, so at the end we can swap start = swapIdx
+            swapIdx++;
+            // Each time through, swap swapIdx with i: 2 <-> 8 then 1 <-> 8... etc. Refer to console.
+            // Which at the very end should place the 'start' at the correct spot
+            swap(arr,swapIdx,i); 
+            console.log(arr);
+        }
+    }
+    swap(arr,start,swapIdx); // Now finally, after loop, swap start with swapIdx
+    return swapIdx;
 }
 
 console.log(pivot([4,8,2,1,5,7,6,3])); // Picking '4' should return index of 3
