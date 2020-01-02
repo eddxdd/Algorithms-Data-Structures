d5985c5de0dcb2a3666ca1fe58a2b19869744b59
Eduardo Lemos <eddu1930@gmail.com>
2020-01-02 17:46:36 -0500

    Update

--- Sorting Algorithms/quickSort.js
+++ Sorting Algorithms/quickSort.js
@@ -31,4 +31,15 @@ function pivot(arr, start=0, end=arr.length-1) {
     return swapIdx;
 }
 
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if (left < right) {
+        let pivotIndex = pivot(arr, left, right);
+        // left
+        quickSort(arr, left, pivotIndex-1);
+        // right
+        quickSort(arr, pivotIndex+1, right);
+    }
+    return arr;
+}
+
 console.log(pivot([4,8,2,1,5,7,6,3])); // Picking '4' should return index of 3
