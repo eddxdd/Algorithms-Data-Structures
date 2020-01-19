f4b698f98435b2b51563b89d80655d670bc23c59
Eduardo Lemos <eddu1930@gmail.com>
2020-01-18 21:25:21 -0500

    Update

--- Data Structures/doublyLinkedList.js
+++ Data Structures/doublyLinkedList.js
@@ -114,8 +114,25 @@ class DoublyLinkedList {
     // 3. If the index is greater than half the length of the list
     // Loop through the list starting from the tail and loop towards the middle
     // Return the node once its found
-    get() {
-        
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
+        var count, current;
+        if (index <= this.length/2) {
+            count = 0;
+            current = this.head;
+            while (count !== index) {
+                current = current.next;
+                count++;
+            }
+        } else {
+            count = this.length - 1;
+            current = this.tail;
+            while (count !== index) {
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
     }
 }
 
