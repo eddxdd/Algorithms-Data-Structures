74b8e0019677130782c3360d2c4c5df9dcbac5f7
Eduardo Lemos <eddu1930@gmail.com>
2020-01-20 22:12:18 -0500

    Update

--- Data Structures/binarySearchTree.js
+++ Data Structures/binarySearchTree.js
@@ -15,4 +15,21 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+
+    // insert() pseudocode
+    // 1. Create a new node
+    // 2. Starting at the root ->
+    // 2a. Check if there is a root, if not - the root now becomes that new node
+    // 2b. If there is a root, check if the value of the new node is greater or less than the value of the root
+    // 3. If it's greater
+    // 3a. Check to see if there is a node to the right
+    // If there is, move to that node and repeat these steps
+    // If there is not, add that node as the right property
+    // 4. If it's less
+    // 4a. Check to see if there is a node to the left
+    // If there is, move to that node and repeat these steps
+    // If there is not, add that node as the left property
+    insert(value) {
+        
+    }
 }
