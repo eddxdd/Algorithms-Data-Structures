e5d81371c53dbfac61f40990f3d3ffb7557283f6
Eduardo Lemos <eddu1930@gmail.com>
2020-01-26 13:53:43 -0500

    Update

--- Data Structures/graph.js
+++ Data Structures/graph.js
@@ -4,6 +4,9 @@
 // Adjacency List: Takes up less space in sparse graphs, faster to iterate over all edges, can be slower to look up specific edge
 // Adjacency Matrix: Takes up more space in sparse graphs, slower to iterate over all edges, but faster to look up specific edge
 
+// * This can be improved via error handling; making sure the vertex is valid, etc.
+// Those are just the bare minimum examples.
+
 class Graph {
     constructor() {
         this.adjacencyList = {};
@@ -20,6 +23,30 @@ class Graph {
     addEdge(v1,v2) {
         this.adjacencyList[v1].push(v2);
         this.adjacencyList[v2].push(v1);
+    }
+    // 1. This function should accept two vertices, we'll call them vertex1 and vertex2
+    // 2. The function should reassign the key of vertex1 to be an array that does not contain vertex2
+    // 3. The function should reassign the key of vertex2 to be an array that does not contain vertex1
+    removeEdge(v1,v2) {
+        // Take the adjacencyList of v1, filter it, keep everything where its not equal to v2
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(
+            v => v !== v2
+        );
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(
+            v => v !== v1
+        );
+    }
+    // 1. The function should accept a vertex to remove
+    // 2. The function loops as long as there are any other vertices in the adjacency list for that vertex
+    // 3. Inside of the loop, call our removeEdge function with the vertex we are removing and any values in the adjacency list for that vertex
+    // 4. Delete the key in the adjacency list for that vertex
+    removeVertex(vertex) {
+        // We'll be popping off values until our list reaches 0 and stops (could also just use a for loop)
+        while (this.adjacencyList[vertex].length) {
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);    // 3.
+        }
+        delete this.adjacencyList[vertex];  // 4.
     }
 }
 
