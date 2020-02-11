3907ffa39e350d2cdd55872c7845ee5ca28005b6
Eduardo Lemos <eddu1930@gmail.com>
2020-02-10 19:21:54 -0500

    Update

---
+++ Challenges/countZeroes.js
@@ -0,0 +1,10 @@
+// Given an array of 1s and 0s which has all 1s first followed by all 0s
+// Write a function called countZeroes() which returns the number of zeroes in the array
+
+function countZeroes(arr, low, high) {
+    var high, low;
+    
+
+}
+
+console.log(countZeroes([1,1,1,1,0,0])); // 2 zeroes
