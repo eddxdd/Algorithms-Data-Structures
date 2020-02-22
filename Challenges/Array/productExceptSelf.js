e41f5ff698e8005c5cc5dca15bb89a6fb0b9d56b
Eduardo Lemos <eddu1930@gmail.com>
2020-02-21 21:21:48 -0500

    Update

--- Challenges/Array/productExceptSelf.js
+++ Challenges/Array/productExceptSelf.js
@@ -26,4 +26,32 @@ var productExceptSelf = function(nums) {
     return output;
 };
 
+var productExceptSelfOptimal = function(nums) {
+    let output = [];
+
+    // output[i] contains the product of all the elements to the left
+    // Note: for the element at index '0', there are no elements to the left,
+    // so the answer[0] would be 1
+    output[0] = 1;
+    for (let i = 1; i < nums.length; i++) {
+        // output[i - 1] already contains the product of elements to the left of 'i - 1'
+        // Simply multiplying it with nums[i - 1] would give the product of all 
+        // elements to the left of index 'i'
+        output[i] = nums[i-1] * output[i-1];
+    }
+
+    // R contains the product of all the elements to the right
+    // Note: for the element at index 'length - 1', there are no elements to the right,
+    // so the R would be 1
+    let R = 1;
+    for (let i = nums.length-1; i >= 0; i--) {
+        // For the index 'i', R would contain the 
+        // product of all elements to the right. We update R accordingly
+        output[i] = output[i] * R;
+        R *= nums[i];
+    }
+    return output;
+}
+
 console.log(productExceptSelf([1,2,3,4]));
+console.log(productExceptSelfOptimal([1,2,3,4]));
