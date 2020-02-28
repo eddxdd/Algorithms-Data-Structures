a5ca821b033c8519db992aeb681ba5ea6e26eb9e
Eduardo Lemos <eddu1930@gmail.com>
2020-02-27 21:08:33 -0500

    Update

--- Challenges/Array/findMin.js
+++ Challenges/Array/findMin.js
@@ -51,18 +51,18 @@ var findMinOptmized = function(nums) {
     return -1;
 }
 
-// function findMinClean(nums) {
-//     let left = 0;
-//     let right = nums.length - 1;
+function findMinClean(nums) {
+    let left = 0;
+    let right = nums.length - 1;
 
-//     while (left < right) {
-//         const mid = Math.floor((left + right) / 2);
-//         if (nums[mid] > nums[right]) left = mid + 1;
-//         else right = mid;
-//     }
-//     return nums[left];
-// }
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] > nums[right]) left = mid + 1;
+        else right = mid;
+    }
+    return nums[left];
+}
 
 // console.log(findMin([3,4,5,1,2]));
 console.log(findMinOptmized([2,1]));
-// console.log(findMinClean([2,1]));
+console.log(findMinClean([2,1]));
