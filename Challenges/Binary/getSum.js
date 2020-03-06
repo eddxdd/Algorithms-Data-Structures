cc10d20fc82a916ae5ca303daea0639e54245b55
Eduardo Lemos <eddu1930@gmail.com>
2020-03-05 21:54:18 -0500

    Update

---
+++ Challenges/Binary/getSum.js
@@ -0,0 +1,9 @@
+// Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+// Input: a = 1, b = 2
+// Output: 3
+
+var getSum = function(a, b) {
+    
+};
+
+console.log(getSum());
